c1b0cc84788d9cbf63f06835c380345d2e0e4dda
Vriddhi05 <kakkarvriddhi035@gmail.com>
2026-02-13 20:02:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Full-Stack-Exp-3-23BAI70621/',  // must match repo name
 })
